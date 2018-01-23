55450330fa8899e8355bd0822dff203166a8e52b
lizziekarr <lizziemoves@gmail.com>
2018-01-22 18:07:16 -0600

    add component for individual appointment and map over appointments in upcoming appointments section

--- src/components/dashboard/messages.js
+++ src/components/dashboard/messages.js
@@ -6,36 +6,49 @@ const messagesArray = [
   {
     date: '2-30-2018',
     sender: 'Doug Funny',
-    subject: 'RE: Test Drive'
+    subject: 'RE: Test Drive',
+    unread: true
   },
   {
     date: '2-29-2018',
     sender: 'Patty Mayonnaise',
-    subject: 'RE: Honda Accord'
+    subject: 'RE: Honda Accord',
+    unread: false
   },
   {
     date: '2-28-2018',
     sender: 'Brandon',
-    subject: 'RE: Honda Civic'
+    subject: 'RE: Honda Civic',
+    unread: false
   },
   {
     date: '2-26-2018',
     sender: 'Susan',
-    subject: 'Honda Civic'
+    subject: 'Honda Civic',
+    unread: false
   }
 ]
 
 const messageList = messagesArray.map((message, index) => {
+
   let background = '#ffffff'
+  let fontWeight = 'regular'
+
   if (index % 2 !== 0) {
     background = '#d8eff4'
   }
+  if (message.unread) {
+    fontWeight = 'bold'
+    console.log('bold');
+  }
+
   return <Message
     key={index}
     date={message.date}
     sender={message.sender}
     subject={message.subject}
-    background={background}/>
+    background={background}
+    fontWeight={fontWeight}/>
 })
 
 const Messages = (props) => {
