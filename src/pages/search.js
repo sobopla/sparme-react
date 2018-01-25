776a5bbfd3347b608a3dc0f3284433a558289a7c
lizziekarr <lizziemoves@gmail.com>
2018-01-25 17:40:27 -0600

    initial redux and redux-form for basic search

--- src/pages/search.js
+++ src/pages/search.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 // import * as actions from '../redux/actions'
 
+import SearchOne from '../containers/search/basic-search'
+
 
 class Search extends Component {
   render(){
     return (
       <div className='search-page'>
-        <div>Initial Search Typeform here</div>
+        <SearchOne />
         <div className='search-button-block'>
           <NavLink to='/advanced-search'>
             <button className='basic-button blue3-border'>Advanced Search</button>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import textReducer from './textReducer'
+import valuesReducer from './valuesReducer'
 import resultsReducer from './resultsReducer'
 import authenticationReducer from './authentication'
 
 export default combineReducers({
 	text: textReducer,
 	results: resultsReducer,
-	authentication: authenticationReducer
+	authentication: authenticationReducer,
+	values: valuesReducer,
+	form: formReducer
 })
