a715b97f2e4deb0512954e00ac3ed24b85d57f4f
lizziekarr <lizziemoves@gmail.com>
2018-01-29 18:24:26 -0600

    add scroll functionality and active classes.  added commments for more info

--- src/components/buttons/OK.js
+++ src/components/buttons/OK.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-
-class Checkbox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checked: props.checked
-    };
-  }
-
-  _handleChange = () => {
-    this.setState({
-      checked: !this.state.checked
-    });
-  };
-
-  render() {
-    const { checked } = this.state;
-    return (
-      <div className="OK">
-        <label>
-          <input
-            type="checkbox"
-            className="OK--input"
-            checked={checked}
-            onChange={this._handleChange}
-          />
-          <span className="OK--span" />
-        </label>
-      </div>
-    );
-  }
-}
-
-export default Checkbox;
+// import React, { Component } from 'react';
+//
+// class Checkbox extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       checked: props.checked
+//     };
+//   }
+//
+//   _handleChange = () => {
+//     this.setState({
+//       checked: !this.state.checked
+//     });
+//   };
+//
+//   render() {
+//     const { checked } = this.state;
+//     return (
+//       <div className="OK">
+//         <label>
+//           <input
+//             type="checkbox"
+//             className="OK--input"
+//             checked={checked}
+//             onChange={this._handleChange}
+//           />
+//           <span className="OK--span" />
+//         </label>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default Checkbox;
