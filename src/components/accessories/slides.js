0999b8d052af979d395b8db68cc31cd4afdf1617
lizziekarr <lizziemoves@gmail.com>
2018-02-05 17:05:08 -0600

    factor out Accessory Slide component and data for slides

---
+++ src/components/accessories/slides.js
@@ -0,0 +1,40 @@
+const Slides = [
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/aftermarket-leather-3x5.jpg',
+    name: 'Aftermarket Leather',
+    price: '$1300'
+  },
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/aftermarket-wheels-3x5.jpg',
+    name: 'Aftermarket Wheels',
+    price: '$1300'
+  },
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/cargomat-3x5.jpg',
+    name: 'Cargo Mat',
+    price: '$95'
+  },
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/floormats-3x5.jpg',
+    name: 'Floor Mats',
+    price: '$195'
+  },
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/pinstripe-3x5.jpg',
+    name: 'Pinstripe',
+    price: '$75'
+  },
+  {
+    url:
+      'https://s3.amazonaws.com/assets.how2car/images/accessories/window-tinting-3x5.jpg',
+    name: 'Window Tinting',
+    price: '$299'
+  }
+];
+
+export default Slides
--- src/containers/accessories/accessories-block.js
+++ src/containers/accessories/accessories-block.js
@@ -1,64 +1,7 @@
 import React, { Component } from 'react';
-import Checkbox from '../../components/buttons/checkbox';
+import Slides from '../../components/accessories/slides'
+import AccessorySlide from '../../components/accessories/accessorySlide'
 
-const Slides = [
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/aftermarket-leather-3x5.jpg',
-    name: 'Aftermarket Leather',
-    price: '$1300'
-  },
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/aftermarket-wheels-3x5.jpg',
-    name: 'Aftermarket Wheels',
-    price: '$1300'
-  },
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/cargomat-3x5.jpg',
-    name: 'Cargo Mat',
-    price: '$95'
-  },
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/floormats-3x5.jpg',
-    name: 'Floor Mats',
-    price: '$195'
-  },
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/pinstripe-3x5.jpg',
-    name: 'Pinstripe',
-    price: '$75'
-  },
-  {
-    url:
-      'https://s3.amazonaws.com/assets.how2car/images/accessories/window-tinting-3x5.jpg',
-    name: 'Window Tinting',
-    price: '$299'
-  }
-];
-
-// Component for slide
-class AccessorySlide extends Component {
-  render() {
-    return (
-      <li className="accessory-slide">
-        <div className="accessory-slide-div">
-          <img src={this.props.slide.url} alt={this.props.slide.name} />
-          <h1 className="accessory-name">{this.props.slide.name}</h1>
-          <h2 className="accessory-price">{this.props.slide.price}</h2>
-          <div className="flex-center">
-            <Checkbox />
-          </div>
-        </div>
-      </li>
-    );
-  }
-}
-
-// Carousel component
 export default class AccessoriesBlock extends Component {
   constructor(props) {
     super(props);
