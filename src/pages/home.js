9d593e5080d5a41633ce3133d39fb0a35e657b74
lizziekarr <lizziemoves@gmail.com>
2018-02-05 16:49:13 -0600

    add home page

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import requireAuth from '../hoc/requireAuth'
+
+const Home = () => {
+  return (
+    <div>
+      <div style={{paddingTop: '200px'}}>Home </div>
+    </div>
+  )
+}
+
+export default Home
