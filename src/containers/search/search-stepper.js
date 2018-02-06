687fa4c66f557c5403b91f66d400c58ff2077950
lizziekarr <lizziemoves@gmail.com>
2018-02-06 15:34:15 -0600

    messing around with functions

--- src/containers/search/search-stepper.js
+++ src/containers/search/search-stepper.js
@@ -21,12 +21,16 @@ import { stepOptions } from './step-functions'
 /**
  * A basic vertical non-linear implementation
  */
+
+let modelOptionsByType, modelOptionsByMake
+
 class SearchStepper extends React.Component {
 
   state = {
     stepIndex: 0,
     makeOptions: makeOptions,
-    modelOptions: modelOptions
+    modelOptions: modelOptions,
+    errors: {zipcode: null, type: null, make: null}
   };
 
   filter = (previousValues, filterBy, nextOptions, newArray) => {
@@ -40,20 +44,60 @@ class SearchStepper extends React.Component {
     })
   }
 
+  filterByType = () => {
+    if (this.props.types !== undefined && Object.keys(this.props.types).some(k =>  this.props.types[k])) {
+      modelOptionsByType = []
+      this.filter(this.props.types, 'type', this.state.modelOptions, modelOptionsByType)
+      this.setState({ stepIndex: 2, modelOptions: modelOptionsByType, errors: {type: ''} })
+    }
+    else {
+      this.setState({errors: {type: 'Please select a type'}})
+    }
+  }
+
+  filterByMake = () => {
+    if ((this.props.types !== undefined) && (this.props.makes !== undefined)) {
+      this.filterByType()
+      modelOptionsByMake = []
+      this.filter(this.props.makes, 'make', this.state.modelOptions, modelOptionsByMake )
+      this.setState({stepIndex: 3, modelOptions: modelOptionsByMake, errors: {make: ''}})
+    }
+    else {
+      this.setState({errors: {make: 'Please select a make'}})
+    }
+  }
+
+  handleZip = (index) => {
+    if (this.props.zipcode !== undefined && this.props.zipcode.length > 4) {
+      this.setState({stepIndex: index, errors: {zipcode: null}})
+    }
+    else {
+      this.setState({errors: {zipcode: 'Zipcode must have at least 5 numbers'}})
+    }
+  }
+
+  // handleStepClick = (index) => {
+  //   const {stepIndex} = this.state
+  //   switch(index){
+  //     case 0: this.setState({stepIndex: 0, modelOptions: modelOptions})
+  //             break
+  //     case 1:
+  //     this.setState({stepIndex: 1, modelOptions: modelOptions})
+  //             break
+  //     case 2: this.filterByMake()
+  //             break
+  //   }
+  // }
+
   handleNext = () => {
     const {stepIndex} = this.state
-    if (stepIndex === 1) {
-      let modelOptionsByType = []
-      this.filter(this.props.types, 'type', this.state.modelOptions, modelOptionsByType)
-      this.setState({ modelOptions: modelOptionsByType })
-    }
-    if (stepIndex === 2) {
-      let modelOptionsByMake = []
-      this.filter(this.props.makes, 'make', this.state.modelOptions, modelOptionsByMake )
-      this.setState({makes: false, models: true, modelOptions: modelOptionsByMake})
-    }
-    if (stepIndex < 3) {
-      this.setState({stepIndex: stepIndex + 1})
+    switch(stepIndex){
+      case 0: this.handleZip()
+              break
+      case 1: this.filterByType()
+              break
+      case 2: this.filterByMake()
+              break
     }
   };
 
@@ -78,16 +122,18 @@ class SearchStepper extends React.Component {
     return (
       <div style={{margin: '12px 0'}}>
         {step < 3 && <RaisedButton
+          backgroundColor="#85d0d4"
+          labelColor='#ffffff'
           label="Next"
           disableTouchRipple={true}
           disableFocusRipple={true}
-          primary={true}
           onClick={this.handleNext}
           style={{marginRight: 12}}
         />}
         {step > 0 && (
           <FlatButton
             label="Back"
+            labelColor="#85d0d4"
             disableTouchRipple={true}
             disableFocusRipple={true}
             onClick={this.handlePrev}
@@ -106,13 +152,18 @@ class SearchStepper extends React.Component {
       <Form onSubmit={()=>handleSubmit(this.props.formSubmit)}>
       <div style={{maxWidth: '90vw', margin: 'auto'}}>
         <Stepper
+          className='custom-stepper'
           activeStep={stepIndex}
           linear={false}
           orientation="vertical"
         >
           <Step>
             <StepButton onClick={() => this.setState({stepIndex: 0})}>
-              Zipcode
+              <div className='custom-step-button'
+                style={this.state.stepIndex === 0 ? null : {color: 'rgba(32, 32, 32, 0.35)'}}
+                iconStyle={{color: '#22a9a3'}}>
+                Zipcode
+              </div>
             </StepButton>
             <StepContent>
               <Field
@@ -122,29 +173,41 @@ class SearchStepper extends React.Component {
                 component={TextField}
               />
               {this.renderStepActions(0)}
+              <div style={{color: '#f16151'}}>{this.state.errors.zipcode}</div>
             </StepContent>
           </Step>
           <Step>
             <StepButton onClick={() => this.setState({stepIndex: 1})}>
-              Select Vehicle Type
+              <div className='custom-step-button'
+                style={this.state.stepIndex === 1 ? null : {color: 'rgba(32, 32, 32, 0.35)'}}>
+                Select Vehicle Type
+              </div>
             </StepButton>
             <StepContent>
               <div>{stepOptions(typeOptions, 'types')}</div>
               {this.renderStepActions(1)}
+              <div style={{color: '#f16151'}}>{this.state.errors.type}</div>
             </StepContent>
           </Step>
           <Step>
-            <StepButton onClick={() => this.setState({stepIndex: 2})}>
-              Select Make
+            <StepButton onClick={() => {this.setState({stepIndex: 2})}}>
+              <div className='custom-step-button'
+                style={this.state.stepIndex === 2 ? null : {color: 'rgba(32, 32, 32, 0.35)'}}>
+                Select Make
+              </div>
             </StepButton>
             <StepContent>
               <div>{stepOptions(makeOptions, 'makes')}</div>
               {this.renderStepActions(2)}
+              <div style={{color: '#f16151'}}>{this.state.errors.make}</div>
             </StepContent>
           </Step>
           <Step>
             <StepButton onClick={() => this.setState({stepIndex: 3})}>
-              Select Model
+              <div className='custom-step-button'
+                style={this.state.stepIndex === 3 ? null : {color: 'rgba(32, 32, 32, 0.35)'}}>
+                Select Model
+              </div>
             </StepButton>
             <StepContent>
               <div>{stepOptions(this.state.modelOptions, 'models')}</div>
--- src/index.js
+++ src/index.js
@@ -7,10 +7,43 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import { Provider } from 'react-redux'
 
+//full list of default color schemes:
+//https://github.com/mui-org/material-ui/blob/master/src/styles/getMuiTheme.js
+
 const muiTheme = getMuiTheme({
   palette: {
     textColor: '#85d0d4',
-  }
+  },
+  stepper: {
+    iconColor: '#85d0d4'
+  },
+  checkbox: {
+    boxColor: '#85d0d4',
+    checkedColor: '#85d0d4',
+    requiredColor: '#f16151',
+    disabledColor: 'rgba(32, 32, 32, 0.35)',
+    labelColor: '#202020',
+    labelDisabledColor: 'rgba(32, 32, 32, 0.35)',
+  },
+  textField: {
+    textColor: '#22a9a3',
+    hintColor: 'rgba(32, 32, 32, 0.35)',
+    floatingLabelColor: 'rgba(32, 32, 32, 0.35)',
+    disabledTextColor: 'rgba(32, 32, 32, 0.35)',
+    errorColor: '#f16151',
+    focusColor: '#85d0d4',
+    backgroundColor: 'transparent',
+    borderColor: 'rgba(32, 32, 32, 0.35)',
+  },
+    datePicker: {
+      color:  '',
+      textColor: '',
+      calendarTextColor: '',
+      selectColor: '',
+      selectTextColor: '',
+      calendarYearBackgroundColor: '',
+      headerColor: ''
+    }
 });
 
 ReactDOM.render(<Provider store={store}><MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider></Provider>, document.getElementById('root'))
